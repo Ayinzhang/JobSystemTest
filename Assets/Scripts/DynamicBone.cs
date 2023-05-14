b5ee55c9ba78b396ed7c5c81f2946ce2f9550e33
TimeTraverler <1242857339@qq.com>
2023-05-14 12:39:46 +0800

    Update

--- Assets/Scripts/DynamicBone.cs
+++ Assets/Scripts/DynamicBone.cs
@@ -22,6 +22,10 @@ namespace DynamicBone
         public quaternion m_PrevRotation;
         public quaternion m_LocalRotation;
         public quaternion m_InitLocalRotation;
+        // prepare data
+        public float3 m_TransformPosition;
+        public float3 m_TransformLocalPosition;
+        public float4x4 m_TransformLocalToWorldMatrix;
     }
 
     public class DynamicBone : MonoBehaviour
@@ -38,7 +42,7 @@ namespace DynamicBone
         int m_Size = 0;
         float m_DeltaTime = 0;
         float m_ObjectScale;
-        float3 m_ObjectMove;
+        static float3 m_ObjectMove;
         float3 m_ObjectPrevPosition;
 
         NativeArray<Particle> m_Particles;
@@ -76,7 +80,7 @@ namespace DynamicBone
         {
             InitTransforms initTransforms = new InitTransforms();
             initTransforms.ps = m_Particles;
-            JobHandle handle = initTransforms.Schedule(m_Size, 4);
+            JobHandle handle = initTransforms.Schedule(m_Size, 16);
             handle.Complete();
         }
 
@@ -95,10 +99,17 @@ namespace DynamicBone
 
         void LateUpdate()
         {
+            //Prepare()
             m_ObjectScale = Mathf.Abs(transform.lossyScale.x);
             m_ObjectMove = (float3)transform.position - m_ObjectPrevPosition;
             m_ObjectPrevPosition = transform.position;
 
+            Prepare prepare = new Prepare();
+            prepare.ps = m_Particles; m_TransformArray = new TransformAccessArray(m_Transforms);
+            JobHandle handle = prepare.Schedule(m_TransformArray);
+            handle.Complete();
+
+            //UpdateParticles()
             int loop = 1;
 
             if (m_UpdateRate > 0)
@@ -123,26 +134,48 @@ namespace DynamicBone
                 for (int i = 0; i < loop; ++i)
                 {
                     UpdateParticles1 updateParticles1 = new UpdateParticles1();
-                    updateParticles1.ps = m_Particles; m_TransformArray = new TransformAccessArray(m_Transforms);
-                    JobHandle handle = updateParticles1.Schedule(m_TransformArray);
+                    updateParticles1.ps = m_Particles;
+                    handle = updateParticles1.Schedule(m_Size, 16);
                     handle.Complete();
-                    //UpdateParticles2();
+
+                    UpdateParticles2 updateParticles2 = new UpdateParticles2();
+                    updateParticles2.ps = m_Particles;
+                    handle = updateParticles2.Schedule(m_Size, 16);
+                    handle.Complete();
                 }
             }
             else
             {
-                //SkipUpdateParticles();
+                SkipUpdateParticles skipUpdateParticles = new SkipUpdateParticles();
+                skipUpdateParticles.ps = m_Particles;
+                handle = skipUpdateParticles.Schedule(m_Size, 16);
+                handle.Complete();
             }
 
             //ApplyParticlesToTransforms();
+
         }
 
         [BurstCompile]
-        struct UpdateParticles1 : IJobParallelForTransform
+        struct Prepare : IJobParallelForTransform
         {
             public NativeArray<Particle> ps;
 
             public void Execute(int i, TransformAccess t)
+            {
+                Particle p = ps[i];
+                p.m_TransformPosition = t.position;
+                p.m_TransformLocalPosition = t.localPosition;
+                p.m_TransformLocalToWorldMatrix = t.localToWorldMatrix;
+            }
+        }
+
+        [BurstCompile]
+        struct UpdateParticles1 : IJobParallelFor
+        {
+            public NativeArray<Particle> ps;
+
+            public void Execute(int i)
             {
                 Particle p = ps[i];
                 if (p.m_ParentIndex >= 0)
@@ -155,7 +188,156 @@ namespace DynamicBone
                 else
                 {
                     p.m_PrevPosition = p.m_Position;
-                    p.m_Position = t.position;
+                    p.m_Position = p.m_TransformPosition;
+                }
+            }
+        }
+
+        [BurstCompile]
+        struct UpdateParticles2 : IJobParallelFor
+        {
+            public NativeArray<Particle> ps;
+
+            public void Execute(int i)
+            {
+                if (i == 0) return;
+
+                Particle p = ps[i];
+                Particle p0 = ps[p.m_ParentIndex];
+
+                float restLen;
+                restLen = math.length(p0.m_TransformPosition - p.m_TransformPosition);
+
+                // keep shape
+                float4x4 m0 = p0.m_TransformLocalToWorldMatrix;
+                m0.c3.xyz = p0.m_Position;
+                float3 restPos;
+                restPos = math.mul(m0, new float4(p.m_TransformLocalPosition, 1)).xyz;
+
+                float3 d = restPos - p.m_Position;
+                p.m_Position += d * p.m_Elasticity;
+
+                d = restPos - p.m_Position;
+                float len = math.length(d);
+                float maxlen = restLen * (1 - p.m_Stiffness) * 2;
+                if (len > maxlen)
+                    p.m_Position += d * ((len - maxlen) / len);
+
+                // keep length
+                float3 dd = p0.m_Position - p.m_Position;
+                float leng = math.length(dd);
+                if (leng > 0)
+                    p.m_Position += dd * ((leng - restLen) / leng);
+            }
+        }
+
+        [BurstCompile]
+        struct SkipUpdateParticles : IJobParallelFor
+        {
+            public NativeArray<Particle> ps;
+
+            public void Execute(int i)
+            {
+                Particle p = ps[i];
+                if (p.m_ParentIndex >= 0)
+                {
+                    p.m_PrevPosition += m_ObjectMove;
+                    p.m_Position += m_ObjectMove;
+
+                    Particle p0 = ps[p.m_ParentIndex];
+
+                    float restLen;
+                    restLen = math.length(p0.m_TransformPosition - p.m_TransformPosition);
+
+                    // keep shape
+                    float4x4 m0 = p0.m_TransformLocalToWorldMatrix;
+                    m0.c3.xyz = p0.m_Position;
+                    float3 restPos;
+                    restPos = math.mul(m0, new float4(p.m_TransformLocalPosition, 1)).xyz;
+
+                    float3 d = restPos - p.m_Position;
+                    p.m_Position += d * p.m_Elasticity;
+
+                    d = restPos - p.m_Position;
+                    float len = math.length(d);
+                    float maxlen = restLen * (1 - p.m_Stiffness) * 2;
+                    if (len > maxlen)
+                        p.m_Position += d * ((len - maxlen) / len);
+
+                    // keep length
+                    float3 dd = p0.m_Position - p.m_Position;
+                    float leng = math.length(dd);
+                    if (leng > 0)
+                        p.m_Position += dd * ((leng - restLen) / leng);
+                }
+                else
+                {
+                    p.m_PrevPosition = p.m_Position;
+                    p.m_Position = p.m_TransformPosition;
+                }
+            }
+        }
+
+        void ApplyParticlesToTransforms()
+        {
+            Vector3 ax = Vector3.right;
+            Vector3 ay = Vector3.up;
+            Vector3 az = Vector3.forward;
+            bool nx = false, ny = false, nz = false;
+
+#if !UNITY_5_4_OR_NEWER
+        // detect negative scale
+        Vector3 lossyScale = transform.lossyScale;
+        if (lossyScale.x < 0 || lossyScale.y < 0 || lossyScale.z < 0)
+        {
+            Transform mirrorObject = transform;
+            do
+            {
+                Vector3 ls = mirrorObject.localScale;
+                nx = ls.x < 0;
+                if (nx)
+                    ax = mirrorObject.right;
+                ny = ls.y < 0;
+                if (ny)
+                    ay = mirrorObject.up;
+                nz = ls.z < 0;
+                if (nz)
+                    az = mirrorObject.forward;
+                if (nx || ny || nz)
+                    break;
+
+                mirrorObject = mirrorObject.parent;
+            }
+            while (mirrorObject != null);
+        }
+#endif
+
+            for (int i = 0; i < m_ParticleTrees.Count; ++i)
+            {
+                ApplyParticlesToTransforms(m_ParticleTrees[i], ax, ay, az, nx, ny, nz);
+            }
+        }
+
+        void ApplyParticlesToTransforms(Vector3 ax, Vector3 ay, Vector3 az, bool nx, bool ny, bool nz)
+        {
+            for (int i = 1; i < m_Particles.Count; ++i)
+            {
+                Particle p = m_Particles[i];
+                Particle p0 = m_Particles[p.m_ParentIndex];
+
+                if (p0.m_ChildCount <= 1)       // do not modify bone orientation if has more then one child
+                {
+                    Vector3 localPos;
+                    localPos = p.m_Transform.localPosition;
+                    Vector3 v0 = p0.m_Transform.TransformDirection(localPos);
+                    Vector3 v1 = p.m_Position - p0.m_Position;
+                    Quaternion rot = Quaternion.FromToRotation(v0, v1);
+                    p0.m_Transform.rotation = rot * p0.m_Transform.rotation;
+                }
+
+                if (p.m_TransformNotNull)
+                {
+                    p.m_Transform.position = p.m_Position;
                 }
             }
         }
